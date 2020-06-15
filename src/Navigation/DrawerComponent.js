877da2dd5147b226fc16c5a29a687e9e74ac808a
John Kolden <john.kolden@gmail.com>
2020-06-14 19:33:44 -0700

    removed unnecessary components

--- src/Navigation/DrawerComponent.js
+++ src/Navigation/DrawerComponent.js
@@ -3,19 +3,13 @@ import clsx from "clsx";
 import { makeStyles } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Drawer from "@material-ui/core/Drawer";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
+
 import List from "@material-ui/core/List";
-import Typography from "@material-ui/core/Typography";
 import Divider from "@material-ui/core/Divider";
 import IconButton from "@material-ui/core/IconButton";
-import Badge from "@material-ui/core/Badge";
-import Link from "@material-ui/core/Link";
-import MenuIcon from "@material-ui/icons/Menu";
+
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
-import NotificationsIcon from "@material-ui/icons/Notifications";
 import { mainListItems, secondaryListItems } from "../listitems";
-import CreateDialogue from "../CreateDialogue";
 import { Route } from "react-router-dom";
 
 const drawerWidth = 240;
